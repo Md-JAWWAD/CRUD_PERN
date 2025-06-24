9485a9862564c599f56f5e6672926255a2e1a636
Muhammad Jawwad <themdjawwad@gmail.com>
2025-06-24 16:09:04 +0500

    Delete routes directory

--- routes/route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const userRoute = require("./user.route");
-const productRoute = require("./product.route");
-const route = Router();
-
-route.use("/user", userRoute);
-route.use("/product", productRoute);
-
-module.exports = route;
