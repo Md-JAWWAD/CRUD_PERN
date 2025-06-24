a17d0c82aea5845e2badd85d087e414c1c1f8757
MD-JAWWAD <themdjawwad@gmail.com>
2025-06-24 21:04:23 +0500

    routes added for login signup

--- Backend folder/routes/route.js
+++ Backend folder/routes/route.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const userRoute = require("./user.route");
 const productRoute = require("./product.route");
+const loginRoute = require("./login.route");
 const route = Router();
 
+route.use("/signup", userRoute);
+route.use("/login", loginRoute);
 route.use("/user", userRoute);
 route.use("/product", productRoute);
 
