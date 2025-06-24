dcaef6aa99f9c9653258babb90300d7afcfa25b8
MD-JAWWAD <themdjawwad@gmail.com>
2025-06-24 21:05:24 +0500

    added user verification middleware

--- Backend folder/routes/user.route.js
+++ Backend folder/routes/user.route.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const userController = require('../controllers/user.controller');
-const route = Router()
+const userController = require("../controllers/user.controller");
+const userVerify = require("../middlewares/user.auth.middleware");
+const route = Router();
 
-route.get('/get-all-user', userController.getAllUsers)
-route.post('/create-user', userController.createUser)
-route.put('/update-user/:id', userController.updateUser)
-route.delete('/delete-user/:id', userController.deleteUser)
+route.get("/get-all-user", userVerify, userController.getAllUsers);
+route.post("/create-user", userVerify, userController.createUser);
+route.put("/update-user/:id", userVerify, userController.updateUser);
+route.delete("/delete-user/:id", userVerify, userController.deleteUser);
 
 module.exports = route;
